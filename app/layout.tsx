da35bb84ef5b0dd8abf2594d606507aaad487b56
mhasan0505 <mhasan.coder@gmail.com>
2025-11-28 17:25:32 +0600

    laptop responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 import "./globals.css";
 import Header from "@/components/application/Header";
 const poppins = Poppins({
@@ -23,6 +25,7 @@ export default function RootLayout({
       <body className={`${poppins.variable} antialiased`}>
         <Header />
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
