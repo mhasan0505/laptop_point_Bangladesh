16a739151c9547d7a5c4d46575207ef1e27d0139
mhasan0505 <mhasan.coder@gmail.com>
2026-01-25 22:12:31 +0600

    fix:pixel set up

--- app/layout.tsx
+++ app/layout.tsx
@@ -179,32 +179,23 @@ export default function RootLayout({
         {/* Facebook Pixel - Deferred */}
         {process.env.NEXT_PUBLIC_FB_PIXEL_ID && (
           <>
-            <Script
-              dangerouslySetInnerHTML={{
-                __html: `
-                  !function(f,b,e,v,n,t,s)
-                  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                  n.queue=[];t=b.createElement(e);t.async=!0;
-                  t.src=v;s=b.getElementsByTagName(e)[0];
-                  s.parentNode.insertBefore(t,s)}(window, document,'script',
-                  'https://connect.facebook.net/en_US/fbevents.js');
-                  fbq('init', '${process.env.NEXT_PUBLIC_FB_PIXEL_ID}');
-                  fbq('track', 'PageView');
-                `,
-              }}
-              strategy="lazyOnload"
-            />
-            <noscript>
-              <img
-                height="1"
-                width="1"
-                className="hidden"
-                src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_FB_PIXEL_ID}&ev=PageView&noscript=1`}
-                alt="Facebook Pixel"
-              />
-            </noscript>
+{/* <!-- Meta Pixel Code --> */}
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1676617903511293');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1676617903511293&ev=PageView&noscript=1"
+/></noscript>
+{/* <!-- End Meta Pixel Code --> */}
           </>
         )}
       </head>
